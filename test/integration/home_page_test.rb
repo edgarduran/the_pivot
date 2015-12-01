b18408c287b76fa32cd72b99bec43f16882bb370
Torie Joy-Warren <victoria.joywarren@gmail.com>
2015-12-01 13:20:31 -0700

    Update homepage test

--- test/integration/home_page_test.rb
+++ test/integration/home_page_test.rb
@@ -1,12 +1,14 @@
-require 'test_helper'
+require "test_helper"
 
 class HomePageTest < ActionDispatch::IntegrationTest
-  test 'visitor can view homepage' do
-    skip
-    # do we even need to create this for it to work? commented out for now
-    # create_featured_car
-    visit '/'
+  test "visitor can view homepage" do
+    create_featured_car
+    visit "/"
 
-    assert page.has_content?('Your Dream Car for a Day')
+    assert page.has_content?("Your Dream Car for a Day")
+    assert page.has_content?("Today's Hottest Wheels!")
+    assert page.has_content?("Details")
+    assert page.has_content?("The El Camino from Dave's Cars")
+    assert page.has_link?("Rent This Car")
   end
 end
