57a312fffd0c23cd14dbb7306f07c5910829a501
David Stinnette <dastinnette@gmail.com>
2015-12-03 13:27:59 -0700

    Pass category model test replacing item with car and category with location

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,26 +1,28 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  
 
-  test 'a category with all attributes is valid' do
-    category = Category.new(title: 'Snowboards')
 
-    assert category.valid?
+  test 'a location with all attributes is valid' do
+    location = Location.new(name: 'Five Points')
+
+    assert location.valid?
   end
 
-  test 'a category without a title is invalid' do
-    category = Category.new(title: '')
+  test 'a location without a name is invalid' do
+    location = Location.new(name: '')
 
-    refute category.valid?
+    refute location.valid?
   end
 
-  test 'a category has items' do
-    create_categories_and_cars
+  test 'a location has cars' do
+    create_cars(4)
+    location  = Location.first
+    location2 = Location.last
 
-    assert_equal 2, @category.items.count
-    assert_equal 2, @category_two.items.count
-    assert_equal 'gnar possum', @category.items.first.name
-    assert_equal 'Sweet Jacket', @category_two.items.first.name
+    assert_equal 2, location.cars.count
+    assert_equal 2, location2.cars.count
+    assert_equal "1960 Chevy0 El Camino0", location.cars.first.full_name
+    assert_equal "1961 Chevy1 El Camino1", location2.cars.first.full_name
   end
 end
