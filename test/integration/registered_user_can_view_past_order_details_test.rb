aeb33c7342d633250be4ec88bde7b5ce852772b9
David Stinnette <dastinnette@gmail.com>
2015-12-03 11:52:46 -0700

    Pass view past order details test - Consolidated test setup with create_cars and login_user - Replaced item with car and table headings in order show - Straight crushed this pomo

--- test/integration/registered_user_can_view_past_order_details_test.rb
+++ test/integration/registered_user_can_view_past_order_details_test.rb
@@ -2,18 +2,14 @@ require 'test_helper'
 
 class RegisteredUserCanViewPastOrderDetailsTest < ActionDispatch::IntegrationTest
   test 'user can view past order details' do
-    create_categories_and_cars
-    current_user = create_user
-    visit login_path
-
-    fill_in 'Username', with: 'Matt'
-    fill_in 'Password', with: 'gnargnar'
-    click_button 'Login'
+    create_cars(1)
+    login_user
+    current_user = User.first
 
     order = current_user.orders.create(current_status: 'ordered',
                                        total_price: 1335)
 
-    order.order_items.create(item_id: Car.first.id, order_id: order.id, quantity: 2)
+    order.order_items.create(car_id: Car.first.id, order_id: order.id, days: 2)
 
     visit '/orders'
 
@@ -26,27 +22,23 @@ class RegisteredUserCanViewPastOrderDetailsTest < ActionDispatch::IntegrationTes
     assert page.has_content?('Submitted on')
 
     within('.orders-table') do
-      assert page.has_content?('Gnar possum')
-      assert page.has_content?('$1000')
-      assert page.has_content?('$2000')
+      assert page.has_content?('1960 Chevy0 El Camino0')
+      assert page.has_content?('$100')
+      assert page.has_content?('$200')
       assert page.has_content?('2')
     end
   end
 
   test 'user can view past order details for multiple items' do
-    create_categories_and_cars
-    current_user = create
-    visit login_path
-
-    fill_in 'Username', with: 'Matt'
-    fill_in 'Password', with: 'gnargnar'
-    click_button 'Login'
+    create_cars(2)
+    login_user
+    current_user = User.first
 
     order = current_user.orders.create(current_status: 'ordered',
                                        total_price: 1335)
 
-    order.order_items.create(item_id: Car.first.id, order_id: order.id, quantity: 2)
-    order.order_items.create(item_id: Car.last.id, order_id: order.id, quantity: 10)
+    order.order_items.create(car_id: Car.first.id, order_id: order.id, days: 2)
+    order.order_items.create(car_id: Car.last.id, order_id: order.id, days: 10)
 
     visit '/orders'
 
@@ -57,14 +49,15 @@ class RegisteredUserCanViewPastOrderDetailsTest < ActionDispatch::IntegrationTes
     assert page.has_content?('Order Status')
     assert page.has_content?('Total')
     assert page.has_content?('Submitted on')
-
+    save_and_open_page
     within('.orders-table') do
-      assert page.has_content?('Gnar possum')
-      assert page.has_content?('Hoody')
-      assert page.has_content?('$1000')
-      assert page.has_content?('$80')
-      assert page.has_content?('10')
-      assert page.has_content?('$2800')
+      assert page.has_content?('1960')
+      assert page.has_content?('1961')
+      assert page.has_content?('$100')
+      assert page.has_content?('$101')
+      assert page.has_content?('$200')
+      assert page.has_content?('$1010')
+      assert page.has_content?('$1210')
     end
   end
 end
