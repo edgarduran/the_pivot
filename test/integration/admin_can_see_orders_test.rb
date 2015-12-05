e82d4306ffe2ee180e34d823f3f0f34556ae75a3
Torie Joy-Warren <victoria.joywarren@gmail.com>
2015-12-05 13:15:21 -0700

    Update unregistered user can't view orders test

--- test/integration/admin_can_see_orders_test.rb
+++ test/integration/admin_can_see_orders_test.rb
@@ -1,18 +1,18 @@
-require 'test_helper'
+require "test_helper"
 
 class AdminCanSeeOrdersTest < ActionDispatch::IntegrationTest
-  test 'logged in platform admin sees dashboard page with all orders' do
+  test "logged in platform admin sees dashboard page with all orders" do
     create_user
     create_user_order(1)
     login_platform_admin
 
-    assert page.has_content?('Welcome, Admin!')
+    assert page.has_content?("Welcome, Admin!")
 
-    within('.all-orders') do
-      assert page.has_content?('Status')
-      assert page.has_content?('Total Price')
-      assert page.has_content?('$200')
-      assert page.has_link?('View Order')
+    within(".all-orders") do
+      assert page.has_content?("Status")
+      assert page.has_content?("Total Price")
+      assert page.has_content?("$200")
+      assert page.has_link?("View Order")
     end
   end
 
@@ -21,17 +21,17 @@ class AdminCanSeeOrdersTest < ActionDispatch::IntegrationTest
     login_store_admin
   end
 
-  test 'platform admin sees number of orders by status' do
+  test "platform admin sees number of orders by status" do
     create_user
     create_user_order(2)
 
     login_platform_admin
 
-    within('.by-status') do
-      assert page.has_content?('Ordered')
-      assert page.has_content?('Canceled')
-      assert page.has_content?('Completed')
-      assert page.has_content?('2')
+    within(".by-status") do
+      assert page.has_content?("Ordered")
+      assert page.has_content?("Canceled")
+      assert page.has_content?("Completed")
+      assert page.has_content?("2")
     end
   end
 end
