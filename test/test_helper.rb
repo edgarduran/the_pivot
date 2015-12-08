dc15acde20cbfba619d131ab01f2268e50e5363f
Edgar Duran <edgarduran@Edgars-MacBook-Pro-5.local>
2015-12-07 20:50:59 -0700

    fix heroku stuff

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+# require 'rails/test_help'
 require 'capybara/rails'
 require 'mocha/mini_test'
 
