5a0a583f3bb36a7eff107cd43f08797b3f97edf8
Torie Joy-Warren <victoria.joywarren@gmail.com>
2015-12-08 14:45:04 -0700

    Uncomment methods in application helper
    - store_admin? and registered_user? uncommented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,11 @@ module ApplicationHelper
     current_user && current_user.platform_admin?
   end
 
-  # def store_admin?
-  #   current_user && current_user.store_admin?
-  # end
-  #
-  # def registered_user?
-  #   current_user && current_user.registered_user?
-  # end
+  def store_admin?
+    current_user && current_user.store_admin?
+  end
+
+  def registered_user?
+    current_user && current_user.registered_user?
+  end
 end
