d5cc82117cb2a4c08130141a4e78a3558e5454e1
Torie Joy-Warren <victoria.joywarren@gmail.com>
2015-12-09 09:47:49 -0700

    Add store owner's cars to their profile page

--- test/integration/guest_views_user_profile_test.rb
+++ test/integration/guest_views_user_profile_test.rb
@@ -1,6 +1,16 @@
 require "test_helper"
 
 class GuestViewsUserProfileTest < ActionDispatch::IntegrationTest
+  def login_store_admin
+    Store.first.update(approved: true)
+    User.find_by(username: "Dave").roles.create(name: 'store_admin')
+
+    visit login_path
+    fill_in 'Username', with: 'Dave'
+    fill_in 'Password', with: 'cars'
+    click_button 'Login'
+  end
+
   test "guest can see store owner's profile" do
     create_cars(1)
 
@@ -14,15 +24,27 @@ class GuestViewsUserProfileTest < ActionDispatch::IntegrationTest
 
     assert_equal "/profile/#{store.owner.id}", current_path
     assert page.has_content?(store.owner.username)
+    assert page.has_content?("Available Cars")
+    assert page.has_content?("1960 Chevy0 El Camino0")
   end
 
   test "store owner can view users' profiles" do
     skip
-    # we need to imlement the "car owner approves requests" before we can do this
+    create_user
+    create_user_order(1)
+    login_store_admin
+    user = User.find_by(username: "Matt")
 
-    # As a store owner,
-    # when I click on a user,
-    # I expect to see their public profile.
-    # then I expect to see the owners profile/description/available cars
+    click_link "Matt"
+
+    assert_equal "/profile/#{user.id}", current_path
+    assert page.has_content?("Order History")
+    assert page.has_content?("Car Ordered")
+    assert page.has_content?("Ordered From")
+    assert page.has_content?("Status")
+
+    assert page.has_content?("1960 Chevy0 El Camino0")
+    assert page.has_link?("Dave's Cars")
+    assert page.has_content?("Pending")
   end
 end
