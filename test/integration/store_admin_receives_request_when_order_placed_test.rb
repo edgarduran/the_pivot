6111b3bdcb97c0058a5023db0d73b5b019fbecfa
Torie Joy-Warren <victoria.joywarren@gmail.com>
2015-12-08 22:31:53 -0700

    Pass decline order test
       - no additional changes were needed for
       this test to pass after the accept order
       test passed

--- test/integration/store_admin_receives_request_when_order_placed_test.rb
+++ test/integration/store_admin_receives_request_when_order_placed_test.rb
@@ -24,6 +24,7 @@ class StoreAdminReceivesRequestWhenOrderPlacedTest < ActionDispatch::Integration
     assert_equal "/#{admin.store.slug}/dashboard/#{admin.store_id}", current_path
     assert page.has_content?("Pending Orders")
     assert page.has_content?("Matt")
+    assert_equal "pending", Order.first.current_status
   end
 
   test "store admin can approve pending orders" do
@@ -37,6 +38,7 @@ class StoreAdminReceivesRequestWhenOrderPlacedTest < ActionDispatch::Integration
     assert_equal "/#{admin.store.slug}/dashboard/#{admin.store_id}", current_path
     assert page.has_content?("Pending Orders")
     assert page.has_content?("Matt")
+    assert_equal "pending", order.current_status
 
     click_link "Approve Order"
 
@@ -45,19 +47,20 @@ class StoreAdminReceivesRequestWhenOrderPlacedTest < ActionDispatch::Integration
   end
 
   test "store admin can decline pending orders" do
-    skip
     create_user
     create_user_order(1)
     login_store_admin
 
     order = Order.first
+    admin = User.find_by(username: "Dave")
 
     assert page.has_content?("Pending Orders")
     assert page.has_content?("Matt")
+    assert_equal "pending", order.current_status
 
     click_link "Decline Order"
 
     assert_equal "/#{admin.store.slug}/dashboard/#{admin.store_id}", current_path
-    assert_equal "canceled", order.current_status
+    assert_equal "canceled", order.reload.current_status
   end
 end
